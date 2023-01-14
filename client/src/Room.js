74fc9d5ec62cfa62c5b4e7c0662ea93ebf871204
DDhanushka <dimuthudhanushka8@gmail.com>
2023-01-14 17:04:02 +0530

    view own video

--- client/src/Room.js
+++ client/src/Room.js
@@ -1,21 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
+import { Peer } from "peerjs";
 
 const Room = () => {
+	const peer = new Peer("pick-an-id");
+	const myVid = useRef();
 	const [roomId, setRoomId] = useState("");
 
-	useEffect(() => {
-		async function getRoomId() {
-			try {
-				const response = await axios.get("http://localhost:3030");
-				console.log(response);
-				if (response.status === 200) {
-					setRoomId(JSON.stringify(response.data));
-				}
-			} catch (error) {
-				console.error(error);
+	async function getRoomId() {
+		try {
+			const response = await axios.get("http://localhost:3030");
+			console.log(response);
+			if (response.status === 200) {
+				setRoomId(JSON.stringify(response.data));
 			}
+		} catch (error) {
+			console.error(error);
 		}
+	}
+
+	const getMedia = () => {
+		navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(
+			(stream) => {
+				const call = peer.call("another-peers-id", stream);
+				call.on("stream", (remoteStream) => {
+					// Show stream in some <video> element.
+				});
+				myVid.current.srcObject = stream;
+			}
+			// ,
+			// (err) => {
+			// 	console.error("Failed to get local stream", err);
+			// }
+		);
+	};
+
+	useEffect(() => {
+		getMedia();
 		getRoomId();
 	}, []);
 
@@ -23,6 +44,8 @@ const Room = () => {
 		<div>
 			<h1>Client:room</h1>
 			<p>{roomId}</p>
+
+			<video playsInline ref={myVid} width="480" height="300" autoPlay  muted/>
 		</div>
 	);
 };
